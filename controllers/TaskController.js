e5b278aa6432367594da6fcd4da31e5d613d3ca7
BereketZz <bereketzewde989@gmail.com>
2024-09-06 07:27:02 +0300

    comment

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -34,19 +34,21 @@ const getTask= async (req, res)=>{
         res.status(400).json({error:"Error Fetching Task!"})
     }
 }
-const getTaskById= async (req, res)=>{
-
-    try{
-        const task= await Task.find({_id:req.params.id,user:req.user._id})
+const getTaskById = async (req, res) => {
+    try {
+         
+        const task = await Task.findOne({ _id: req.params.id, user: req.user._id });
+        
         if (!task) {
             return res.status(404).json({ error: 'Task not found' });
         }
-        res.status(200).json(task)
-
-    }catch(error){
-        res.status(400).json({error:"Error Fetching Task!"})
+        
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(400).json({ error: 'Error Fetching Task!' });
     }
-}
+};
+
 const updateTask= async (req,res)=>{
     try {
         const task = await Task.findOneAndUpdate(
@@ -87,10 +89,8 @@ const stateTask= async (req,res)=>{
         // Count the number of completed tasks for the user
         const completedTasks = await Task.countDocuments({ user: req.user._id, status: 'completed' });
         
-        // You can add more stats like pending tasks, etc.
         const pendingTasks = await Task.countDocuments({ user: req.user._id, status: 'pending' });
 
-        // Return the stats in the response
         res.status(200).json({
             totalTasks,
             completedTasks,
